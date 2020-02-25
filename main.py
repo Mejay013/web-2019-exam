fe70e5073db04854e2f0b5b513c8574a3972ce0d
Mejay <mail.dev012@gmail.com>
2020-02-25 18:55:08 +0300

    msfm,dsm,fdsm,

--- main.py
+++ main.py
@@ -55,3 +55,84 @@ def set(id):
         user_list = User.query.all()
         role = Role
         return render_template('set.html',books_list=books_list,issue=issue,status_list=status_list,user=user,role=role,user_list=user_list)
+
+@main.route('/set2/<id>',methods=['POST','GET'])
+def set2(id):
+    if request.method == 'POST':
+        bad_issue= Issue_log.query.filter_by(id=id).first()
+
+        if not bad_issue:
+            books_list = Books
+            issue_list = Issue_log.query.order_by(Issue_log.date_log.desc())
+            status_list = Status
+            user = User
+            users_list = User.query.all()
+            role = Role
+            error = "Изменения уже приступили в силу"
+            return render_template('issue.html',books_list=books_list,users_list=users_list,issue_list=issue_list,status_list=status_list,user=user,role=role,error = error)
+
+
+        new_date = bad_issue.date_log
+        new_book = bad_issue.book_log
+        user_id = bad_issue.user_log
+
+        new_status = request.form['new_status']
+
+        db.session.delete(bad_issue)
+        new_issue = Issue_log(date_log=new_date , user_log=user_id ,book_log=new_book,status_log=new_status )
+        db.session.add(new_issue)
+        db.session.commit()
+        return render_template('index.html')
+    else:
+        issue = Issue_log.query.filter_by(id = id).first()
+        books_list = Books
+        status_list = Status.query.all()
+        status = Status
+        user = User
+        user_list = User.query.all()
+        role = Role
+        return render_template('set.html',books_list=books_list,issue=issue,status_list=status_list,user=user,role=role,user_list=user_list)
+
+@main.route('/delete/<id>',methods=['POST','GET'])
+def delete(id):
+    bad_appeal = Appeal.query.filter_by(id=id).first()
+    if not bad_appeal:
+        return redirect(url_for('main.index'))
+    db.session.delete(bad_appeal)
+    db.session.commit()
+    return redirect(url_for('main.index'))
+
+@main.route('/add/<id>',methods=['POST','GET'])
+def add(id):
+    if request.method == "POST":
+        new_user = request.form['new_user']
+        new_data = datetime.datetime.now()
+        check_issue = Issue_log.query.filter_by(user_log = new_user).first()
+        
+        if check_issue:
+            books_list = Books
+            issue_list = Issue_log.query.order_by(Issue_log.date_log.desc())
+            status_list = Status
+            user = User
+            users_list = User.query.all()
+            role = Role
+            error = "Изменения уже приступили в силу"
+            return render_template('issue.html',books_list=books_list,users_list=users_list,issue_list=issue_list,status_list=status_list,user=user,role=role,error = error)
+        
+        issue_status = 1
+        new_book = id
+        new_issue = Issue_log(date_log=new_data , user_log=new_user ,book_log=new_book,status_log=issue_status )
+    
+        db.session.add(new_issue)
+        db.session.commit()
+
+    else:
+        book = Books.query.filter_by(id = id).first()
+        issue = Issue_log
+        status_list = Status.query.all()
+        status = Status
+        user = User
+        user_list = User.query.all()
+        role = Role
+        return render_template('add_issue.html',book=book,issue=issue,status_list=status_list,user=user,role=role,user_list=user_list)
+
